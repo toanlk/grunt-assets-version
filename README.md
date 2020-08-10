ac6f419e77c196ee01cc9e3b2c7286b4bb98a7ab
Le Khanh Toan <toanlk@bestprice.vn>
2020-08-10 15:23:35 +0700

    Update source

--- README.md
+++ README.md
@@ -17,14 +17,14 @@ Once the plugin has been installed, it may be enabled inside your Gruntfile with
 grunt.loadNpmTasks('grunt-assets-version');
 ```
 
-## The "assets_version" task
+## The "version" task
 
 ### Overview
-In your project's Gruntfile, add a section named `assets_version` to the data object passed into `grunt.initConfig()`.
+In your project's Gruntfile, add a section named `version` to the data object passed into `grunt.initConfig()`.
 
 ```js
 grunt.initConfig({
-  assets_version: {
+  version: {
     options: {
       // Task-specific options go here.
     },
--- tasks/assets_version.js
+++ tasks/assets_version.js
@@ -21,7 +21,7 @@ module.exports = function (grunt) {
 		return hash.digest(encoding);
 	}
 
-	grunt.registerMultiTask('assets_version', 'Static file asset revisioning through content hashing', function () {
+	grunt.registerMultiTask('version', 'Static file asset revisioning through content hashing', function () {
 		var options = this.options({
 			encoding: 'utf8',
 			algorithm: 'md5',
@@ -31,10 +31,13 @@ module.exports = function (grunt) {
 		this.files.forEach(function (filePair) {
 			filePair.src.forEach(function (f) {
 
-				var hash = md5(f, options.algorithm, 'hex', options.encoding),
-					prefix = hash.slice(0, options.length),
-					renamed = [path.basename(f), prefix].join('.'),
-					outPath = path.resolve(path.dirname(f), renamed);
+				var hash = md5(f, options.algorithm, 'hex', options.encoding);
+				var prefix = hash.slice(0, options.length);
+				var arrName = path.basename(f).split(".");
+				var ext = arrName.pop();
+				var fileName = arrName.join('.');
+				var renamed = [fileName, prefix, ext].join('.');
+				var outPath = path.resolve(path.dirname(f), renamed);
 
 				grunt.verbose.ok().ok(hash);
 				fs.renameSync(f, outPath);
